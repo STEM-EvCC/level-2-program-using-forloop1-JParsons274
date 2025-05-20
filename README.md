b256efe9116aff9fe32b6f3eb8175e7a19758af1
JParsons274 <jtparsons@students.everettcc.edu>
2025-05-20 21:37:24 +0000

    made space program work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# STEM103_Level3_1-ForLoop
+#   Jack Parsons 
+# 5/20/2025 STEM103_Level3_1-ForLoop
+# this program uses a loop to count space missions, check the success rate, and list the pre 2000's launches
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
+# mission counters
+total_missions = 0
+successful_missions = 0 
+pre_2000_missions = [] 
+
+# process data
+for i in range(len(mission_names)):
+    total_missions += 1
+    if mission_success[i]:
+        successful_missions += 1
+        if mission_years[i] < 2000:
+            pre_2000_missions.append(mission_names[i])
+
+# calc success rate 
+success_rate = (successful_missions / total_missions) * 100
+
+#output
+print(f"Total number of missions: {total_missions}")
+print(f"Number of successful missions: {successful_missions}")
+print(f"Success rate: {success_rate:.2f}%")
+print("Missions launched before the year 2000:")
+
+for mission in pre_2000_missions: 
+    print(f"-{mission}")
+    
